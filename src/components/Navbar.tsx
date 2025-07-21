0063c99999570788f6102367545bce7c0ea94601
James Hughes <james.a.hughes@gmail.com>
2025-07-21 13:58:25 -0500

    update ts on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import logo from "../assets/images/logo.png";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  const linkClass = ({ isActive }: { isActive: boolean }) =>
+  const linkClass = ({ isActive }: { isActive: boolean }): string =>
     isActive
       ? "text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
       : "text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2";
